1631d1935bb5381f23069ff2291977677824102c
Rosie Liu <rosie.liurx@gmail.com>
2021-11-05 19:45:28 -0400

    草稿

--- rest.py
+++ rest.py
@@ -10,7 +10,7 @@ To run locally:
 import os
 from sqlalchemy import *
 from sqlalchemy.pool import NullPool
-from flask import Flask, request, render_template, g, redirect, Response
+from flask import Flask, request, render_template, g, redirect, Response, url_for
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
@@ -48,10 +48,55 @@ def teardown_request(exception):
     except Exception as e:
         pass
 
-@app.route('/')
+@app.route('/', methods=('GET', 'POST'))
 def index():
+    if request.method == 'POST':
+        areas = request.form.get('Areas')
+        cuisines = request.form.get('Cuisines')
+        dine_types = request.form.get('Dine_Type')
+        dine_options = request.form.get('Dine_Option')
+        return redirect(url_for('webshow', areas = areas, cuisines = cuisines, dine_types = dine_types, dine_options = dine_options))
+    
     area_selection = engine.execute('SELECT * FROM Area').fetchall()
     cuisine_selection = engine.execute('SELECT * FROM').fetchall()
     dine_type_selection = engine.execute('SELECT * FROM').fetchall()
     dine_option_selection = engine.execute('SELECT * FROM').fetchall()
     return render_template('index.html', area_selection, cuisine_selection, dine_option_selection, dine_type_selection)
+
+@app.route('/show_result/?')
+def webshow(areas, cuisines, dine_types, dine_options):
+    restaurants = engine.execute('select r.restaurant_name, r.rid, a.street_name'
+                                'from Restaurant r join Rest_Type c on r.rid = c.rid'
+                                'join Located_in l on r.rid = l.rid'
+                                'join Address a on l.sid = a.sid'
+                                'join Restaurant_Dining_Type dt on r.rid = dt.rid'
+                                'join Restaurant_Dining_Option do on r.rid = do.rid'
+                                'where c.cid = ? and a.aid = ? and dt.tid = ? and do.option_id = ?',
+                                (cuisines, areas, dine_types, dine_options))
+    return render_template('webshow.html', restaurants)
+
+if __name__ == "__main__":
+  import click
+
+  @click.command()
+  @click.option('--debug', is_flag=True)
+  @click.option('--threaded', is_flag=True)
+  @click.argument('HOST', default='0.0.0.0')
+  @click.argument('PORT', default=8111, type=int)
+  def run(debug, threaded, host, port):
+    """
+    This function handles command line parameters.
+    Run the server using:
+
+        python server.py
+
+    Show the help text using:
+
+        python server.py --help
+
+    """
+
+    HOST, PORT = host, port
+    print("running on %s:%d" % (HOST, PORT))
+    app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
+  run()
